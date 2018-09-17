8ab4819b15ddfc0e5a01b4b778e00ca5e9f37ebc
kimyunje <stoneaxe002@gmail.com>
2018-09-17 17:25:34 +0900

    9월 17일 실습

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,9 +1,11 @@
 // jQuery를 활용한 인터렉션 ...
 var item = $('.menu-item');
 var subItem = $('.sub-menu a');
+var tab = $('.tab');
 item.attr('tabindex', '0');
+tab.attr('tabindex', '0');
 subItem.attr("class", 'fas fa-angle-left');
-
+// 메인 메뉴를 위한 함수
 item.on('mouseover focusin', function(){
     item.removeClass('menu-act');
     $(this).addClass('menu-act');
@@ -15,3 +17,11 @@ subItem.hover(function(){
 
 // addClass 안의 값은 . 찍지 말것.
 // this는 item을 가르킨다. 즉, mouseove, focusin이 된 menu-item을 찾는 것.
+
+// 탭 메뉴를 위한 함수
+tab.on('click focusin', function(){
+    $(this).parent().siblings().removeClass('tab-act');
+    $(this).parent().addClass('tab-act');
+});
+// this는 클릭한 tab
+// parent는 부모요소
